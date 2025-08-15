babf08848e43336fbd37d8d845fbca428ecbf9c0
Mikey Bronowski <MikeyBronowski@gmail.com>
2025-08-15 13:26:13 +0100

    Update sw.js

--- game/sw.js
+++ game/sw.js
@@ -1,19 +1,30 @@
 const CACHE = 'flagquest-v1';
 const ASSETS = [
-  '/', '/index.html', '/flags.html', '/spelling.html',
-  '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'
+  'index.html',
+  'flags.html',
+  'spelling.html',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
 ];
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+
+self.addEventListener('install', e => {
+  e.waitUntil((async () => {
+    const cache = await caches.open(CACHE);
+    await cache.addAll(ASSETS);
+    self.skipWaiting();
+  })());
 });
-self.addEventListener('activate', e=> e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e=>{
-  e.respondWith((async ()=>{
+
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+
+self.addEventListener('fetch', e => {
+  e.respondWith((async () => {
     const cache = await caches.open(CACHE);
     const cached = await cache.match(e.request);
     if (cached) return cached;
     try {
-      const res = await fetch(e.request, {cache:'no-store'});
+      const res = await fetch(e.request, { cache: 'no-store' });
       if (res && res.ok) cache.put(e.request, res.clone());
       return res;
     } catch {
